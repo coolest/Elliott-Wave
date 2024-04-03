fe2e297a193238cbb2f91d52413e520560ced3a3
Dyllenf <dyllenfau@gmail.com>
2024-04-03 14:37:46 -0500

    boilerplate

--- Elliott-Wave-Prediction/src/main/java/AI/App.java
+++ Elliott-Wave-Prediction/src/main/java/AI/App.java
@@ -1,4 +1,7 @@
 package AI;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 /**
  * Hello world!
@@ -6,8 +9,21 @@ package AI;
  */
 public class App 
 {
+    private static void buildGUI(){
+        JFrame frame = new JFrame("Elliott Wave Predictor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Add the "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        // Display the window.
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        buildGUI();
     }
 }
